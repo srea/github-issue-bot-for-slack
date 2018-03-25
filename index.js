30d9e9ba8c3e081498f89711cf503e6d36278c7b
yuki tamazawa <srea@srea.local>
2018-03-25 22:24:30 +0900

    報告者を追加。一度に作れるように

--- index.js
+++ index.js
@@ -21,7 +21,43 @@ controller
     }
   });
 
+controller.hears(new RegExp("^issue作って(.*)", "i"), ["ambient"], function(
+  bot,
+  message
+) {
+  bot.api.reactions.add(
+    {
+      timestamp: message.ts,
+      channel: message.channel,
+      name: "eyes"
+    },
+    function(error, _) {
+      if (error) {
+        bot.botkit.log("Failed to add emoji reaction:", error);
+      }
+    }
+  );
+  var splitText = message.text.split(/\r\n|\r|\n/);
+  var title = splitText[1];
+  splitText.shift();
+  splitText.shift();
+  var body = splitText.join("\n");
+  var labels = obtainRelatedLabels(title + body);
+  createGithubIssue(title, body, labels)
+    .then(function(response) {
+      bot.reply(message, "作成しました\n" + response.html_url);
+    })
+    .catch(function(error) {
+      convo.say("error");
+    });
+});
+
 controller.hears("(.*)", ["direct_mention", "mention"], function(bot, message) {
+  var from;
+  bot.api.users.info({ user: message.user }, function(err, info) {
+    from = info.user.name;
+  });
+
   var askTitle = function(err, convo) {
     convo.ask("タイトルを記入してください", function(response, convo) {
       askDescription(response, convo);
@@ -32,7 +68,7 @@ controller.hears("(.*)", ["direct_mention", "mention"], function(bot, message) {
     var title = response.text;
 
     convo.ask("コメントを記入してください", function(response, convo) {
-      var body = response.text;
+      var body = "@" + from + "より\n\n" + response.text;
       var labels = obtainRelatedLabels(title + body);
       createGithubIssue(title, body, labels)
         .then(function(response) {
