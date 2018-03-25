ae6e02adebc7c74fd83c39ba9e1d44f7fed33e68
yuki tamazawa <srea@srea.local>
2018-03-25 21:28:55 +0900

    promise化、labels自動付加

--- index.js
+++ index.js
@@ -1,73 +1,91 @@
-const Botkit = require('botkit');
-const Request = require('request');
+const Botkit = require("botkit");
+const Request = require("request");
+var rp = require("request-promise");
+
+var createIssue = function(title, body, labels) {
+  var options = {
+    uri: "https://api.github.com/repos/srea/github-issue-bot-for-slack/issues",
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "User-Agent": "Bot"
+    },
+    auth: {
+      user: process.env.GITHUB_USER,
+      password: process.env.GITHUB_ACCESS_TOKEN
+    },
+    body: {
+      title: title,
+      body: body,
+      labels: labels
+    },
+    json: true
+  };
+
+  return rp(options);
+};
+
+const LabelPattern = {
+  iOS: /iOS|iPhone/i,
+  Android: /Android/i,
+  Server: /Server|Web|サーバー|ウェブ/i,
+  Bug: /Bug|不具合|バグ|問題|クラッシュ/i
+};
+
+var obtainRelatedLabels = function(text) {
+  var labels = [];
+  for (label in LabelPattern) {
+    if (text.match(LabelPattern[label])) {
+      console.log(LabelPattern[label]);
+      labels.push(label);
+    }
+  }
+  return labels;
+};
 
 if (!process.env.SLACK_BOT_TOKEN) {
-    console.log('Error: Specify token in environment');
-    process.exit(1);
+  console.log("Error: Specify token in environment");
+  process.exit(1);
 }
 
 const controller = Botkit.slackbot({
-    debug: false
+  debug: false
 });
 
-controller.spawn({
+controller
+  .spawn({
     token: process.env.SLACK_BOT_TOKEN
-}).startRTM(function (err) {
+  })
+  .startRTM(function(err) {
     if (err) {
-        throw new Error(err);
+      throw new Error(err);
     }
-});
+  });
 
-controller.hears('a(.*)', ['direct_message', 'direct_mention', 'mention'], function (bot, message) {
-    var bot = bot;
-    var askFlavor = function (err, convo) {
-        convo.ask('タイトルをお願いします。', function (response, convo) {
-            askSize(response, convo);
-            convo.next();
+controller.hears("(.*)", ["direct_mention", "mention"], function(bot, message) {
+  var askTitle = function(err, convo) {
+    convo.ask("タイトルを記入してください", function(response, convo) {
+      askDescription(response, convo);
+      convo.next();
+    });
+  };
+  var askDescription = function(response, convo) {
+    var title = response.text;
+
+    convo.ask("コメントを記入してください", function(response, convo) {
+      var body = response.text;
+      var labels = obtainRelatedLabels(title + body);
+      createIssue(title, body, labels)
+        .then(function(response) {
+          convo.say("作成しました\n" + response.html_url);
+          convo.next();
+        })
+        .catch(function(error) {
+          convo.say("error");
+          convo.next();
         });
-    };
-    var askSize = function (response, convo) {
-
-        var title = response.text;
-
-        convo.ask('詳細な説明をお願いします。', function (response, convo) {
-
-            var body = response.text;
-
-            // from BOT Label
-            // add ios label when if ios keyword in message, 
-            //ヘッダーを定義
-            var headers = {
-                'Content-Type': 'application/json',
-                'User-Agent': 'Bot'
-            }
-
-            var json = {
-                "title": title,
-                "body": body
-            }
-
-            //オプションを定義
-            var options = {
-                url: 'https://api.github.com/repos/srea/github-issue-bot-for-slack/issues',
-                method: 'POST',
-                headers: headers,
-                auth: {
-                    user: process.env.GITHUB_USER,
-                    password: process.env.GITHUB_ACCESS_TOKEN,
-                },
-                json: json
-            }
-
-            //リクエスト送信
-            Request(options, function (error, response, body) {
-                console.log(response.html_url);
-            });
-            convo.say("");
-            convo.next();
-        });
-    };
-    bot.startConversation(message, askFlavor);
-    console.log(message.user);
+    });
+  };
+  bot.startConversation(message, askTitle);
+  console.log(message.user);
 });
-
